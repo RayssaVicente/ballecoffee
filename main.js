ece4f217142be4ff9632c12a360452a88060f20a
Rayssa Vicente <rayssavicenteviegas@gmail.com>
2024-09-21 20:43:35 -0300

    Javascript do projeto

---
+++ main.js
@@ -0,0 +1,10 @@
+const botao = document.querySelector('#botao')
+
+botao.addEventListener('click', () => {
+    window.scroll({top: window.innerHeight, behavior: 'smooth'})
+    botao.style.display = 'none'
+})
+
+
+
+
